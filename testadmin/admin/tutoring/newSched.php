18ded56a53af7d6ea9fdf3920faa0c000eff0dd8
Jack Williams <jwilliams17@winona.edu>
2018-12-07 11:43:34 -0600

    Integration done. Looks good for now. Needs more testing.

--- testadmin/admin/tutoring/newSched.php
+++ testadmin/admin/tutoring/newSched.php
@@ -177,4 +177,4 @@
 	fclose($Fhandle);
 ?>
 
-<a href="../../../testFrontEnd/schedule.php">Click here to go to the tutoring page. (file name neeeds to be added)</a>
+<a href="../../../testCSwebsite/current_students/tutoring/schedule.php">Click here to go to the tutoring page. (file name neeeds to be added)</a>
